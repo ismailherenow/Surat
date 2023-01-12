9d3b7523af8fde008157d982b409e8e0899124ef
iSmail <madani.ismail.dev@gmail.com>
2023-01-12 20:54:19 +0100

    Add project files.

---
+++ Surat/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Surat;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Surat/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Surat;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
